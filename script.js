0818cc81481f4404ca7f788d511490bf4671ef7b
vardan <VARDANMANUKYAN9339@gmail.com>
2020-07-07 15:29:29 +0400

    add if

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('How films are you watched ?', '');
 
 const personalMovieDB = {
@@ -9,13 +11,34 @@ const personalMovieDB = {
 };
 
 
-const a = prompt('One of the latest watched films?', ''),
-      b = prompt('Rate film ?',''),
-      c = prompt('One of the latest watched films?', ''),
-      d = prompt('Rate film ?','');
 
 
-personalMovieDB.movies[a] = b;      
-personalMovieDB.movies[c] = d;    
+for (let i = 0; i < 2; i++) {
+
+    const a = prompt('One of the latest watched films?', ''),
+          b = prompt('Rate film ?','');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+       
+        personalMovieDB.movies[a] = b;
+        console.log('done!');       
+
+    } else {
+        console.log('error!');
+        i--;
+    }     
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('less films!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('you are classic watcher');
+} else if (personalMovieDB.count >= 30) {
+    console.log('you are kinoman');
+} else {
+    console.log('error!!!');
+}
 
 console.log(personalMovieDB);
+
